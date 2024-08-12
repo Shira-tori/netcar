a8c1fba082e14684d2df249a1280ca583771547b
Shira-tori <jiyuuomitometeirushiratori@protonmail.com>
2024-08-12 09:34:43 +0800

    about to use argparse

--- netcar.py
+++ netcar.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 import socket
 import sys
+import argparse
 
 args = sys.argv
 
@@ -12,13 +13,8 @@ def client():
     print("client")
 
 if __name__ == '__main__':
-    if len(args) == 4:
-        server()
-    elif len(args) == 3:
-        client()
-    else:
-        print("Usage: ./netcar [-b listen] destination port")
-        print("Example: ")
-        print("\t./netcar -b 0.0.0.0 9999 ")
-        print("\t./netcar www.google.com 9999 ")
+    print("Usage: ./netcar [-b listen] destination port")
+    print("Example: ")
+    print("\t./netcar -b 0.0.0.0 9999 ")
+    print("\t./netcar www.google.com 9999 ")
         
