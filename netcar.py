68f107893343adc4343663c281ae5cfb1bd556ea
Shira-tori <jiyuuomitometeirushiratori@protonmail.com>
2024-08-13 20:47:17 +0800

    added argument parser

--- netcar.py
+++ netcar.py
@@ -4,6 +4,9 @@ import sys
 import argparse
 
 args = sys.argv
+listen = False
+target = ""
+port = 0
 
 def server():
     print("[*] Starting server...")
@@ -12,9 +15,35 @@ def server():
 def client():
     print("client")
 
-if __name__ == '__main__':
+def printUsage():
     print("Usage: ./netcar [-b listen] destination port")
     print("Example: ")
     print("\t./netcar -b 0.0.0.0 9999 ")
     print("\t./netcar www.google.com 9999 ")
+
+def parse():
+    global listen
+    global target
+    global port
+
+    if len(args) <= 1 or len(args) > 4:
+        printUsage()
+        exit()
+    for i in args:
+        if i == "-b":
+            listen = True
+            continue
+        elif i.isdigit():
+            port = int(i)
+            continue
+        else:
+            target = i
+
+if __name__ == '__main__':
+    parse()
+    print(listen, target, port)
+        
+        
+        
+
         
